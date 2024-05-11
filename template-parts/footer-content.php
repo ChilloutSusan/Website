f4b1aa59489cec89134e5be8bbe576ab592eff69
Chillout Susan <ChilloutSusan@users.noreply.github.com>
2024-05-11 20:09:39 +0200

    newplace

---
+++ template-parts/footer-content.php
@@ -0,0 +1,6 @@
+<footer class="u-align-center u-clearfix u-footer u-gradient u-footer" id="sec-61d0">
+  <div class="u-clearfix u-sheet u-sheet-1">
+    <p class="u-small-text u-text u-text-variant u-text-1">Copyrighted By @ChilloutSusan<br>
+    </p>
+  </div>
+</footer>
---
+++ template-parts/header-content.php
@@ -0,0 +1,76 @@
+<header class="u-clearfix u-grey-80 u-header u-header" id="sec-60a2">
+  <div class="u-clearfix u-sheet u-sheet-1">
+    <?php
+            ob_start();
+            ?><nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+      <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px; font-weight: 700;">
+        <a class="u-button-style u-custom-border u-custom-border-color u-custom-borders u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+          <svg class="u-svg-link" viewBox="0 0 24 24"><use xlink:href="#menu-hamburger"></use></svg>
+          <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+        </a>
+      </div>
+      <div class="u-custom-menu u-nav-container">
+        {menu}
+      </div>
+      <div class="u-custom-menu u-nav-container-collapse">
+        <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+          <div class="u-inner-container-layout u-sidenav-overflow">
+            <div class="u-menu-close"></div>
+            {responsive_menu}
+          </div>
+        </div>
+        <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+      </div>
+    </nav><?php
+            $menu_template = ob_get_clean();
+            $menuPath = '/template-parts/menu/primary-navigation-1/';
+            $processorPath = get_theme_file_path('/template-parts/menu/popups-render.php');
+            if (file_exists($processorPath)) {
+                include $processorPath;
+            }
+            echo Theme_NavMenu::getMenuHtml(array(
+                'container_class' => 'u-menu u-menu-one-level u-offcanvas u-menu-1',
+                'menu' => array(
+                    'is_mega_menu' => false,
+                    'menu_class' => 'u-nav u-spacing-20 u-unstyled u-nav-1',
+                    'item_class' => 'u-nav-item',
+                    'link_class' => 'u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-palette-2-base',
+                    'link_style' => 'padding: 10px 44px 10px 46px;',
+                    'submenu_class' => 'u-h-spacing-20 u-nav u-unstyled u-v-spacing-10 u-block-672a-31',
+                    'submenu_item_class' => 'u-nav-item',
+                    'submenu_link_class' => 'u-button-style u-nav-link u-white',
+                    'submenu_link_style' => '',
+                ),
+                'responsive_menu' => array(
+                    'is_mega_menu' => false,
+                    'menu_class' => 'u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2',
+                    'item_class' => 'u-nav-item',
+                    'link_class' => 'u-button-style u-nav-link',
+                    'link_style' => '',
+                    'submenu_class' => 'u-h-spacing-20 u-nav u-unstyled u-v-spacing-10 u-block-672a-33',
+                    'submenu_item_class' => 'u-nav-item',
+                    'submenu_link_class' => 'u-button-style u-nav-link',
+                    'submenu_link_style' => '',
+                ),
+                'theme_location' => 'primary-navigation-1',
+                'template' => $menu_template,
+                'mega_menu' => isset($megaMenu) ? $megaMenu : '{}',
+            )); ?>
+    <div class="u-social-icons u-spacing-30 u-social-icons-1">
+      <a class="u-social-url" target="_blank" href="https://www.youtube.com/@PantsuVR" title="Youtube"><span class="u-file-icon u-icon u-social-facebook u-social-icon u-icon-1"><img src="<?php echo get_template_directory_uri(); ?>/images/400474.png" alt=""></span>
+      </a>
+      <a class="u-social-url" target="_blank" href="twitter.com/pantsuvr" title="twitter"><span class="u-icon u-icon-circle u-social-icon u-social-twitter u-text-white u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xlink:href="#svg-0987"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0px" y="0px" id="svg-0987"><path d="M92.2,38.2c0,0.8,0,1.6,0,2.3c0,24.3-18.6,52.4-52.6,52.4c-10.6,0.1-20.2-2.9-28.5-8.2 c1.4,0.2,2.9,0.2,4.4,0.2c8.7,0,16.7-2.9,23-7.9c-8.1-0.2-14.9-5.5-17.3-12.8c1.1,0.2,2.4,0.2,3.4,0.2c1.6,0,3.3-0.2,4.8-0.7 c-8.4-1.6-14.9-9.2-14.9-18c0-0.2,0-0.2,0-0.2c2.5,1.4,5.4,2.2,8.4,2.3c-5-3.3-8.3-8.9-8.3-15.4c0-3.4,1-6.5,2.5-9.2 c9.1,11.1,22.7,18.5,38,19.2c-0.2-1.4-0.4-2.8-0.4-4.3c0.1-10,8.3-18.2,18.5-18.2c5.4,0,10.1,2.2,13.5,5.7c4.3-0.8,8.1-2.3,11.7-4.5 c-1.4,4.3-4.3,7.9-8.1,10.1c3.7-0.4,7.3-1.4,10.6-2.9C98.9,32.3,95.7,35.5,92.2,38.2z"></path></svg></span>
+      </a>
+      <a class="u-social-url" target="_blank" href="https://patreon.com/PantsuVR" title="patreon"><span class="u-icon u-social-icon u-social-instagram u-icon-3"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 51.997 51.997" style=""><use xlink:href="#svg-712f"></use></svg><svg class="u-svg-content" viewBox="0 0 51.997 51.997" x="0px" y="0px" id="svg-712f" style="enable-background:new 0 0 51.997 51.997;"><g><path d="M51.911,16.242C51.152,7.888,45.239,1.827,37.839,1.827c-4.93,0-9.444,2.653-11.984,6.905
+		c-2.517-4.307-6.846-6.906-11.697-6.906c-7.399,0-13.313,6.061-14.071,14.415c-0.06,0.369-0.306,2.311,0.442,5.478
+		c1.078,4.568,3.568,8.723,7.199,12.013l18.115,16.439l18.426-16.438c3.631-3.291,6.121-7.445,7.199-12.014
+		C52.216,18.553,51.97,16.611,51.911,16.242z M49.521,21.261c-0.984,4.172-3.265,7.973-6.59,10.985L25.855,47.481L9.072,32.25
+		c-3.331-3.018-5.611-6.818-6.596-10.99c-0.708-2.997-0.417-4.69-0.416-4.701l0.015-0.101C2.725,9.139,7.806,3.826,14.158,3.826
+		c4.687,0,8.813,2.88,10.771,7.515l0.921,2.183l0.921-2.183c1.927-4.564,6.271-7.514,11.069-7.514
+		c6.351,0,11.433,5.313,12.096,12.727C49.938,16.57,50.229,18.264,49.521,21.261z"></path>
+</g></svg></span>
+      </a>
+    </div>
+  </div>
+</header>
